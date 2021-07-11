41835395a5f410641fb118b9b2aeacf666446779
Daniel Squires <daniel.squires331@gmail.com>
2021-07-11 00:12:52 -0600

    added more reducer state management, styling

--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -4,15 +4,43 @@ import { Link } from 'react-router-dom';
 import ScoreboardContext from './ScoreboardContext';
 import "./Scoreboard.css";
 
-const FIELD_LENGTH = 100;
-const NUM_DOWNS = 4;
-const NUM_QUARTERS = 4;
+// Class name prefixes
 const PRE = 'scoreboard';
+const PRE_ADMIN = PRE + '-admin';
+
+// Constants
+const DOWNS = ['1st', '2nd', '3rd', '4th'];
+const FIELD_LENGTH = 100;
+const TIMEOUTS = ['', '', ''];
+const QUARTERS = ['1st', '2nd', '3rd', '4th'];
 const TEAMS = ['home', 'away'];
 
-
+// State management
 const reducer = (state, action) => {
 	switch(action.type) {
+		case 'changePossession':
+			const teamPossessing = !state.teamPossessing;
+			return { ...state, currentDown: 1, teamPossessing, yardsToFirst: 10,}
+		case 'changeTeamLogo':
+			return initialState;
+		case 'changeTeamName':
+			return { ...state, teamName: action.value };
+		case 'changeTeamInfo':
+			return initialState;
+		case 'changeYardsUntilFirst':
+			return { ...state, yardsToFirst: action.value };
+		case 'decrementTimeout':
+			return initialState;
+		case 'endDown':
+			const currentDown = state.currentDown + 1;
+			if (currentDown > DOWNS.length) {
+				const teamPossessing = !state.teamPossessing;
+				return { ...state, currentDown: 1, teamPossessing, yardsToFirst: 10,}
+			}
+			return { ...state, currentDown}
+		case 'incrementQuarter':
+			const currentQuarter = state.currentQuarter + 1 <= QUARTERS.length ? state.currentQuarter + 1 : 1;
+			return { ...state, currentQuarter};
 		case 'incrementScore':
 			return { ...state, teamData: 
 				{...state.teamData, [action.id]: 
@@ -21,21 +49,9 @@ const reducer = (state, action) => {
 					}
 				}
 			};
-		case 'incrementQuarter':
-			return initialState;
-		case 'changeTeamLogo':
-			return initialState;
-		case 'changeTeamName':
-			return { ...state, teamName: action.value };
-		case 'changePossession':
-			return initialState;
-		case 'changeTeamInfo':
-			return initialState;
-		case 'changeYardsToFirst':
-			return initialState;
-		case 'endDown':
-			return initialState;
 		case 'reset':
+			return initialState;
+		case 'resetTimeouts':
 			return initialState;
 		default:
 			return initialState;
@@ -43,40 +59,47 @@ const reducer = (state, action) => {
 };
 
 
-
+// Rendering
 const Scoreboard = ({ isAdmin }) => {
 	const { scoreboardState, setScoreboardState } = useContext(ScoreboardContext);
 	const [state, dispatch] = useReducer(reducer, scoreboardState);
 	const hasChanges = JSON.stringify(state) !== JSON.stringify(initialState); 
 
-	// Rendering
 	const renderAdminControls = () => {
-		const adminPre = PRE + '-admin';
-
 		return (
-			<div className={`${adminPre}`}>
-				<div className={`${adminPre}-title`}>ADMIN</div>
-				<div className={`${adminPre}-controls`}>
-					<div className={`${adminPre}-team-controls-container`}>
+			<div className={`${PRE_ADMIN}`}>
+				<div className={`${PRE_ADMIN}-title`}>ADMIN</div>
+				<div className={`${PRE_ADMIN}-controls`}>
+					<div className={`${PRE_ADMIN}-team-controls-container`}>
 						{TEAMS.map(renderTeamControls)}
 					</div>
-					<div className={`${adminPre}-general-controls-container`}>
-						<button onClick={() => dispatch({ type: `endDown` })}>End Down</button>
-						<button onClick={() => dispatch({ type: `changeYardsToFirst` })}>Change Yards to First Down</button>
-						<button onClick={() => dispatch({ type: `turnover` })}>Turnover</button>
-						<button className={`${hasChanges ? 'enabled' : 'disabled'}`} onClick={() => setScoreboardState(state)}>save</button>
-						<button className={`${hasChanges ? 'enabled' : 'disabled'}`} onClick={() => dispatch({ type: 'reset' })}>new game</button>
+					<div className={`${PRE_ADMIN}-general-controls-container`}>
+						{renderGeneralControls()}
 					</div>
 				</div>
 			</div>
 		);
 	};
 
-	const renderTeamControls = (team, teamIndex) => {
-		const adminPre = PRE + '-admin';
-						
+	const renderGeneralControls = () => {
 		return (
-			<div className={`${adminPre}-team-controls ${teamIndex === 0 ? 'left' : 'right'}`}>
+			<div className={`${PRE_ADMIN}-general-controls`}>
+				<input placeholder="Enter yards until First Down" onChange={(e) => dispatch({ type: 'changeYardsUntilFirst', value: e.target.value })} />
+				<button onClick={() => dispatch({ type: `endDown` })}>End Down</button>
+				<button onClick={() => dispatch({ type: `incrementQuarter` })}>Increment Quarter</button>
+				<button onClick={() => dispatch({ type: `changeYardsUntilFirst` })}>Change Yards to First Down</button>
+				<button onClick={() => dispatch({ type: `turnover` })}>Turnover</button>
+				<button className={`${hasChanges ? 'enabled' : 'disabled'}`} onClick={() => setScoreboardState(state)}>save</button>
+				<button className={`${hasChanges ? 'enabled' : 'disabled'}`} onClick={() => dispatch({ type: 'reset' })}>new game</button>
+			</div>
+		)
+	}
+
+	const renderTeamControls = (team, teamIndex) => {	
+		console.log({TIMEOUTS});
+		
+		return (
+			<div className={`${PRE_ADMIN}-team-controls ${teamIndex === 0 ? 'left' : 'right'}`}>
 				<button onClick={() => dispatch({ type: `incrementScore`, id: teamIndex, value: 6 })}>Touchdown</button>
 				<button onClick={() => dispatch({ type: `incrementScore`, id: teamIndex, value: 1 })}>Extra Point</button>
 				<button onClick={() => dispatch({ type: `incrementScore`, id: teamIndex, value: 3 })}>Field Goal</button>
@@ -92,16 +115,29 @@ const Scoreboard = ({ isAdmin }) => {
 			<div className={`${PRE}-teams`}>
 				{TEAMS.map((team, teamIndex) => {
 					console.log({state});
-					const { hasPossession, score, teamName } = state.teamData[teamIndex];
+					const { teamInfo, teamLogo, teamName, timeoutsLeft, score } = state.teamData[teamIndex];
 
 					return (
 						<div className={`${PRE}-team-container ${teamIndex === 0 ? 'left' : 'right'}`}>
 							<div className={`${PRE}-team-name`}>{teamName}</div>
+							<div className={`${PRE}-team-logo-timeouts`}>
+								<img className={`${PRE}-team-logo`} src={teamLogo} />
+								<div className={`${PRE}-team-timeouts`}>
+									<div className={`${PRE}-team-timeouts-label`}>Timeouts: </div>
+									{TIMEOUTS.map((to, toIndex) => (
+										<div className={`${PRE}-team-timeout-bubble ${toIndex > timeoutsLeft ? 'empty' : 'filled'}`} />
+									))}
+								</div>
+							</div>
 							<div className={`${PRE}-team-score`}>{score}</div>
 						</div>
 					);
 				})}
 			</div>
+			<div className={`${PRE}-quarter`}>Quarter: {state.currentQuarter}</div>
+			<div className={`${PRE}-down`}>Down: {state.currentDown}</div>
+			<div className={`${PRE}-yards-till-first`}>YTF: {state.yardsToFirst}</div>
+
 			{isAdmin && renderAdminControls()}
 			{!isAdmin &&
 				<Link to='/admin'>
@@ -110,7 +146,7 @@ const Scoreboard = ({ isAdmin }) => {
 			}
 			{isAdmin &&
 				<Link to='/'>
-					<button>Back to Home</button>
+					<button onClick={()=> console.log('worked')}>Back to Home</button>
 				</Link>
 			}
 		</div>
